d149304663f2d2d30da0393ba8b4ffc97ea714f8
gargvishu2000 <45060213+gargvishu2000@users.noreply.github.com>
2025-05-23 11:35:18 +0530

    changed connection code

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -91,10 +91,13 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
+      withCredentials: true,
+      transports: ["websocket", "polling"],
       query: {
         userId: authUser._id,
       },
     });
+    
     socket.connect();
 
     set({ socket: socket });
